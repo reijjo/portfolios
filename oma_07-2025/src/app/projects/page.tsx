10e566b0b4f89f5e8dbd3ea35f3741452753405d
Reijjo <teemu.aitomeri@gmail.com>
2025-07-25 21:01:33 +0300

    basiclayout figured out in projects page

--- oma_07-2025/src/app/projects/page.tsx
+++ oma_07-2025/src/app/projects/page.tsx
@@ -1,10 +1,11 @@
+import Projects from "./_components/Projects";
+import styles from "./page.module.css";
+
 const ProjectsPage = () => {
   return (
-    <section className="projects-page">
-      <h1>Projects</h1>
-      <p>Here you can find a list of my projects.</p>
-      {/* Add your project components here */}
-    </section>
+    <main className={styles.projectPage}>
+      <Projects />
+    </main>
   );
 };
 
