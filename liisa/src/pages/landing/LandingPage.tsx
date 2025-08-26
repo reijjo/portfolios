58692c15ef64fa53f3119879ff8a098c287dc8dc
Reijjo <teemu.aitomeri@gmail.com>
2025-08-26 21:49:57 +0300

    added couple of photos

--- liisa/src/pages/landing/LandingPage.tsx
+++ liisa/src/pages/landing/LandingPage.tsx
@@ -1,10 +1,21 @@
 import "./LandingPage.css";
 
+import hero from "../../assets/images/landing/liisa-hero.jpg";
+import herodesk from "../../assets/images/landing/liisa-desk.jpg";
+
 const LandingPage = () => {
   return (
     <main className="landing-page">
       <section className="hero">
-        <h1>Luisa Lore</h1>
+        <picture className="hero-image">
+          <source media="(width <= 600px)" srcSet={hero} />
+          <img
+            src={herodesk}
+            alt="Luisa Lore"
+            title="Luisa Lore"
+            className="hero-img"
+          />
+        </picture>
       </section>
     </main>
   );
