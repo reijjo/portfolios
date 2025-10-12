2b013255ffd730878a4e8d5631800f7acc07b435
Reijjo <teemu.aitomeri@gmail.com>
2025-10-12 18:04:51 +0300

    whaat

--- nextjsliisa/src/components/layout/Footer.tsx
+++ nextjsliisa/src/components/layout/Footer.tsx
@@ -3,7 +3,7 @@ import "./Footer.css";
 export default function Footer() {
   return (
     <footer className="wrapper">
-      <p>&copy; {new Date().getFullYear()}</p>
+      <p>&copy; {new Date().getFullYear()} Repe</p>
     </footer>
   );
 }
