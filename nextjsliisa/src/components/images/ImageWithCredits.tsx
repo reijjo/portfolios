20466a158ba694cfd7a642b50b9500aa7f8c9ced
Reijjo <teemu.aitomeri@gmail.com>
2026-02-03 21:57:44 +0200

    suggeted fixes

--- nextjsliisa/src/components/images/ImageWithCredits.tsx
+++ nextjsliisa/src/components/images/ImageWithCredits.tsx
@@ -1,6 +1,10 @@
+"use client";
+
 import Image from "next/image";
 import "./ImageWithCredits.css";
 import { Info } from "lucide-react";
+
+import { useId } from "react";
 
 type ImageWithCreditsProps = {
   src: string;
@@ -13,17 +17,21 @@ export default function ImageWithCredits({
   alt,
   credits,
 }: ImageWithCreditsProps) {
+  const popoverId = useId();
+
   return (
     <div className="image-with-credits">
       <Image src={src} alt={alt} fill sizes="(max-width: 768px) 100vw, 50vw" />
       <button
-        popoverTarget="kuvaaja"
+        type="button"
+        aria-label="Näytä kuvaaja"
+        popoverTarget={popoverId}
         popoverTargetAction="show"
         className="show-kuvaaja"
       >
         <Info size={16} />
       </button>
-      <p popover="auto" id="kuvaaja" className="image-credits">
+      <p popover="auto" id={popoverId} className="image-credits">
         Kuva: {credits}
       </p>
     </div>
