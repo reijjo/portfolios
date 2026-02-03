bc5211019dc30bc5e4c8129e82f2a7627c6a268a
Reijjo <teemu.aitomeri@gmail.com>
2026-02-03 17:31:33 +0200

    okok

--- nextjsliisa/src/app/(pages)/esitykset/_components/Mayralakit.tsx
+++ nextjsliisa/src/app/(pages)/esitykset/_components/Mayralakit.tsx
@@ -7,8 +7,37 @@ type MayralakitProps = {
 export default function Mayralakit({ active }: MayralakitProps) {
   return (
     <section className={`${active ? "esitys-section" : "hide-section"}`}>
-      <h2>Mäyrälakit</h2>
-      MÄYRÄÄÄLAKKIIITAAAAA
+      <h1>Mäyrälakit</h1>
+      <div className="jokukuva">joku kuva</div>
+      <p>
+        Mäyrälakit on noin kahdeksan jäsenen muodostama yhtye, joka esittää itse
+        säveltämäänsä ja suomeksi sanoittamaansa kevyttä laulu- ja
+        soitinmusiikkia, jossa on vaikutteita rock-iskelmästä ja
+        vaihtoehtorockista. Mäyrälakkien saundimaailma on orgaanisen juurevaa,
+        ja siinä tuoksuu lämpimät kesäyöt, himmeät kapakat ja elämän
+        katkeransuloinen toivorikkaus.
+      </p>
+      <p>
+        Mäyrälakit on soittanut yhdessä vuodesta 2015 saakka, ja yhtyeen
+        ensimmäinen levy ”Tanssi halki tavernan” julkaistiin vuonna 2021.
+        Debyyttilevynsä jälkeen yhtye on julkaissut pari singleä, ja nyt
+        kymmenvuotista taivaltaan juhlistava yhtye on palannut studioon
+        tarkoituksenaan julkaista toinen pitkäsoitto vuoden 2026 aikana.
+      </p>
+      <div className="jokukuva">joku kuva</div>
+      <p>Lisää tekstii Lorem ipsum dolor sit amet ??</p>
+      <div>
+        Linkit
+        <a
+          href="https://open.spotify.com/artist/34En99VGBVc5hlfYJXbp27?si=41MmFRSNT1eQHwX8Qscrdg&nd=1&dlsi=064a3fddf86440ff"
+          target="_blank"
+        >
+          spotfiy
+        </a>
+        <a href="https://www.instagram.com/mayralakit" target="_blank">
+          Insta
+        </a>
+      </div>
     </section>
   );
 }
--- nextjsliisa/src/app/(pages)/esitykset/_components/Spektri.tsx
+++ nextjsliisa/src/app/(pages)/esitykset/_components/Spektri.tsx
@@ -7,7 +7,44 @@ type SpektriProps = {
 export default function Spektri({ active }: SpektriProps) {
   return (
     <section className={`${active ? "esitys-section" : "hide-section"}`}>
-      <h2>Valosirkus Spektri</h2>
+      <h1>Valosirkus Spektri</h1>
+      <div className="jokukuva">joku kuva</div>
+      <p>
+        Olemme Valosirkus Spektri Helsingistä ja intohimonamme on tanssi
+        erilaisilla led-välineillä! Esitämme uusia ihastusta ja ihmetystä
+        herättäviä itsetekemiämme koreografioita mm. vanteille, siiville,
+        viuhkoille, piiskoille, naruille, palloille, tikuille ja luomme
+        musiikkiin vauhdikkaita esityskomboja, joissa välineet vaihtuu. Välillä
+        rauhoitumme hitaampien melodioiden äärelle viipyillen joissain
+        kauniissa.
+      </p>{" "}
+      <p>
+        Esiinnymme päivän tai öin, mutta vain pimeässä esityksen saa loistamaan
+        kaikissa sateenkaaren väreissä, ja valokarnevaali pääsee täyteen
+        voimaansa.
+      </p>
+      <p>
+        Erilaiset tilaisuudet festarilavoista kaduille ja toreille, bileistä
+        päiväkoteihin, Asunnottomien yöstä Helsingin yliopiston saliin, sekä
+        kaupallisista toimijoista, yksityistilaisuuksiin, ja alakulttuurien
+        pariin eri puolella Suomea, ovat vahvistaneet ammattitaitoamme jo
+        vuodesta 2015 lähtien.
+      </p>
+      <p>
+        Me rakastamme sitä mitä teemme, ja tuommekin mukanamme sanoman ilosta ja
+        hauskanpidosta!
+      </p>
+      <div className="jokukuva">joku kuva</div>
+      <p>Lisää tekstii Lorem ipsum dolor sit amet ??</p>
+      <div>
+        Linkit
+        <a href="https://www.valosirkusspektri.fi/" target="_blank">
+          Website
+        </a>
+        <a href="https://www.instagram.com/valosirkusspektri" target="_blank">
+          Insta
+        </a>
+      </div>
     </section>
   );
 }
